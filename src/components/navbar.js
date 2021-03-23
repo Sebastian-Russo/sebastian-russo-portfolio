ad238bac6a833fa8bd6443ea24b0ceaa03a58681
Sebastian Russo <russo.sebastian@gmail.com>
2021-03-22 22:44:37 -0400

    changed colors and font

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ export const NavBar = () => {
         <input type="checkbox" id="check" />
         <label htmlFor="check" className="hamburger-button"><i className="fas fa-bars"></i></label>
       <ul className="navbar-ul">
-        <div><HashLink smooth to="#contact">Sebastian Russo</HashLink></div>
+        <div><HashLink smooth to="#contact" className="navlink">Sebastian Russo</HashLink></div>
         <div className="navlink"><a href="https://github.com/Sebastian-Russo" rel="noreferrer" target="_blank">Github</a></div>
         <div className="navlink"><HashLink smooth to="#top">Top</HashLink></div>
         <div className="navlink"><HashLink smooth to="#projects">Projects</HashLink></div>
